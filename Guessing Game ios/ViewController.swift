6602468bf412540d0139f082761a1a7a17cc270b
Gabriella Torres <gabrielatorres@Interapts-MacBook-Pro-6.local>
2018-10-10 09:17:01 -0500

    Basic Information

--- Guessing Game ios/ViewController.swift
+++ Guessing Game ios/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var GuessTextField: UITextField!
+    @IBOutlet weak var GuessButton: UIButton!
+    @IBOutlet weak var FeedbackLabel: UILabel!
+    @IBOutlet weak var GuessesLeftLabel: UILabel!
+    
+    @IBAction func GuessButtonTapped(_ sender: Any) {
+       guesses -= 1
+       GuessesLeftLabel.text = "\(guesses) guesses remaining"
+    }
+    
+    var guesses = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+            FeedbackLabel.text = "\(guesses) guesses remaining"
+
     }
 
 
